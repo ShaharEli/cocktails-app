729e749f9c95f412e19b242eac9615296370c2ee
ShaharEli <shahar.e3@gmail.com>
2020-12-28 15:39:51 +0200

    changed package name

--- android/app/src/main/java/com/cocktailsshahar/MainActivity.java
+++ android/app/src/main/java/com/cocktailsshahar/MainActivity.java
@@ -1,4 +1,4 @@
-package com.cocktailsshahar;
+package com.shaharcocktails;
 
 import com.facebook.react.ReactActivity;
 
--- android/app/src/main/java/com/cocktailsshahar/MainApplication.java
+++ android/app/src/main/java/com/cocktailsshahar/MainApplication.java
@@ -1,4 +1,4 @@
-package com.cocktailsshahar;
+package com.shaharcocktails;
 
 import android.app.Application;
 import android.content.Context;
